e64039a30e4a60a17afd9ed08fa0a133dc23750a
Farvaev Emil <farvaev.emil@gmail.com>
2023-06-01 01:19:22 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import classNames from "classnames";
+import { Component } from "solid-js";
+
+const App: Component = () => {
+	return <div class={classNames("h-[100vh] p-2")}>hi</div>;
+};
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import { Config } from "tailwindcss";
+
+const config: Config = {
+	content: ["./src/**/*.{html,js,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
+
+export default config;
